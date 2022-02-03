2ae55b0874f8843ab447c2a8a4ed9d374444d4a4
Byronn Walker-Mills <bwalkermills@gmail.com>
2022-02-03 11:36:06 +0000

    Add components for title and info card

---
+++ src/lib/InfoCard.svelte
@@ -0,0 +1,15 @@
+<div>
+    <p>IP Address</p>
+
+    <p>Location</p>
+
+    <p>Timezone</p>
+
+    <p>UTC</p> <!-- add offset value dynamically using the API -->
+    
+    <p>ISP</p>
+</div>
+
+<style>
+    
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
+    import Title from '$lib/Title.svelte';
     import Search from '$lib/Search.svelte';
+    import InfoCard from '$lib/InfoCard.svelte';
 </script>
 
 <svelte:head>
@@ -7,23 +9,11 @@
 </svelte:head>
 
 <header>
-    <h1>
-        <a href="/">IP Address Tracker</a>
-    </h1>
+    <Title />
 
     <Search />
 
-    <div>
-        <p>IP Address</p>
-
-        <p>Location</p>
-
-        <p>Timezone</p>
-
-        <p>UTC</p> <!-- add offset value dynamically using the API -->
-        
-        <p>ISP</p>
-    </div>
+    <InfoCard />
 </header>
 
 <style>
@@ -31,18 +21,5 @@
         background: url('pattern-bg.png') no-repeat center bottom royalblue;
         background-size: cover;
         padding: 1.625rem 1.5rem;
-    }
-
-    h1 {
-        font-size: 1.625rem;
-        font-weight: 500;
-        text-align: center;
-        margin-top: 0;
-        margin-bottom: 1.75rem;
-    }
-
-    a {
-        display: block;
-        color: white;
     }
 </style>
